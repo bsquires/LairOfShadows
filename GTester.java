efa066cc528c77447405cc90a654b7866b30c04d
Eduardo-Arevalos <arevalos00004@att.net>
2013-11-12 22:20:54 -0800

    I added my GUI code and text files
    Look over what I have so far, if you can better organize or clean the
    code do so, and I inserted my text file for you guys to see, modify it
    and use it as an example if you want.

---
+++ GTester.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+import java.io.*;
+import javax.imageio.*;
+
+public class GTester {
+	public static void main(String[] args)throws IOException{
+        JFrame frame = new Gam_alt();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+	}
+
+}
